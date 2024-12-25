e6a79ed11679eb08253e79985db4232fbcbb2cb3
Zekeriya Damcı <126283115+zekddd@users.noreply.github.com>
2024-12-25 22:21:29 +0300

    Elevator.cpp

--- src/Elevator.cpp
+++ src/Elevator.cpp
@@ -1,25 +1,34 @@
 #include "Elevator.h"
 #include <time.h>
 
+// Constructor to initialize the Elevator object
 Elevator::Elevator(const std::string &deviceName)
-    : SmartDevice(deviceName), maintenanceRequired(false)
+    : SmartDevice(deviceName), maintenanceRequired(false) // Initialize base class and set maintenance status to false
 {
-    this->type = Type::Elevator;
+    this->type = Type::Elevator; // Set the device type to "Elevator"
 }
 
+// Callback function for the elevator button
 void Elevator::deviceCallback(Fl_Widget *widget, void *data)
 {
+    // Cast the `data` pointer to an `Elevator` object
     Elevator *elevator = static_cast<Elevator *>(data);
+
+    // Seed the random number generator with the current time
     srand(time(NULL));
+
+    // Generate a random number between 1 and 100
     int num = rand() % 100 + 1;
-    if (num <= 8)
+
+    // Simulate a maintenance check with an 8% chance of requiring maintenance
+    if (num <= 8) // If the random number is 8 or less
     {
-        addNotification("Elevator maintenance required!");
-        maintenanceRequired = true;
+        addNotification("Elevator maintenance required!"); // Notify the user about maintenance
+        maintenanceRequired = true; // Set the maintenance flag to true
     }
-    else
+    else // Otherwise, the elevator is functioning well
     {
-        addNotification("Elevator is functioning well.");
-        maintenanceRequired = false;
+        addNotification("Elevator is functioning well."); // Notify the user that the elevator is fine
+        maintenanceRequired = false; // Set the maintenance flag to false
     }
 };
